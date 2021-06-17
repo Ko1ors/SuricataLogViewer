98b64c75dd820bb9c4cc2aba2ff6fa20a52e7ec0
2011maxwell2011@ukr.net <2011maxwell2011@ukr.net>
2021-06-17 19:34:57 +0300

    v1

---
+++ SuricataLogViewer/EventList/EventListPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using SuricataLogViewer.Model;
+using SuricataLogViewer.Services;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace SuricataLogViewer.EventList
+{
+    /// <summary>
+    /// Логика взаимодействия для EventListPage.xaml
+    /// </summary>
+    public partial class EventListPage : Page
+    {
+        private List<SuricataEvent> events;
+        private SuricataService suricataService;
+
+        public EventListPage()
+        {
+            InitializeComponent();
+            suricataService = new SuricataService();
+        }
+
+        private void ButtonShowAll_Click(object sender, RoutedEventArgs e)
+        {
+            events = suricataService.GetLog("https://raw.githubusercontent.com/FrankHassanabad/suricata-sample-data/master/samples/wrccdc-2018/alerts-only.json");
+            eventListView.Items.Clear();
+            if (events != null)
+                foreach (var evt in events)
+                {
+                    eventListView.Items.Add(new EventUC(evt));
+                }
+
+        }
+        private void ButtonSearch_Click(object sender, RoutedEventArgs e) { }
+    }
+}
+
+
---
+++ SuricataLogViewer/PageController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Controls;
+
+namespace SuricataLogViewer
+{
+    public static class PageController
+    {
+        private static Dictionary<Type, Page> pagesDictionary = new Dictionary<Type, Page>();
+
+        public static Page GetPageObject<T>() where T : Page, new()
+        {
+            if (!pagesDictionary.TryGetValue(typeof(T), out Page page))
+            {
+                page = new T();
+                pagesDictionary.Add(typeof(T), page);
+            }
+            return page;
+        }
+    }
+}
