190ead91278fce1afc07fac003a475e363636f8f
Ko1ors <kolorsnik181200@gmail.com>
2021-06-17 13:13:32 +0300

    Suricata service added

---
+++ SuricataLogViewer/Services/Request.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuricataLogViewer.Services
+{
+    public static class Request
+    {
+        public static string Send(string request)
+        {
+            string result;
+            using (WebClient webClient = new WebClient())
+            {
+                result = webClient.DownloadString(request);
+            }
+            return result;
+        }
+    }
+}
---
+++ SuricataLogViewer/Services/SuricataService.cs
@@ -0,0 +1,27 @@
+﻿using SuricataLogViewer.Model;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuricataLogViewer.Services
+{
+    public class SuricataService
+    {
+
+        public List<SuricataEvent> GetLog(string url)
+        {
+            try
+            {
+                var result = Request.Send(url);
+                return JsonConvert.DeserializeObject<List<SuricataEvent>>(result);
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
